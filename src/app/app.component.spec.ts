b583920137ec7d5d28a773491f6bb9d1dac44f88
jirayu <jirayu.photisan@gmail.com>
2018-01-23 00:37:48 +0700

    secound pattern right operand

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { Captcha } from './captcha';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
@@ -58,23 +59,64 @@ describe('AppComponent', () => {
 
     });
 
-    describe("Secound pattern left operand", () => {
-      it("test 1 should be one", async(() => {
-        const component = TestBed.createComponent(AppComponent);
+    describe("Secound pattern", () => {
+      describe("left operand", () => {
+        it("test 1 should be one", async(() => {
+          const component = TestBed.createComponent(AppComponent);
 
-        let captcha = component.componentInstance.captcha(2, 1, 1, 1);
+          let captcha = component.componentInstance.captcha(2, 1, 1, 1);
 
-        expect(captcha.leftOperand).toEqual("one");
-      }));
+          expect(captcha.leftOperand).toEqual("one");
+        }));
 
-      it("test 2 should be two", async(() => {
-        const component = TestBed.createComponent(AppComponent);
+        it("test 2 should be two", async(() => {
+          const component = TestBed.createComponent(AppComponent);
 
-        let captcha = component.componentInstance.captcha(2, 2, 1, 1);
+          let captcha = component.componentInstance.captcha(2, 2, 1, 1);
 
-        expect(captcha.leftOperand).toEqual("two");
-      }));
+          expect(captcha.leftOperand).toEqual("two");
+        }));
+
+
+        it("test 3 should be two", async(() => {
+          const component = TestBed.createComponent(AppComponent);
+
+          let captcha = component.componentInstance.captcha(2, 3, 1, 1);
+
+          expect(captcha.leftOperand).toEqual("three");
+        }));
+      });
+
+      describe("right operand", () => {
+        it("test 1 should be 1", async(() => {
+          const component = TestBed.createComponent(AppComponent);
+
+          let captcha = component.componentInstance.captcha(2, 1, 1, 1);
+
+          expect(captcha.rightOperand).toEqual("1");
+        }));
+
+        it("test 2 should be 2", async(() => {
+          const component = TestBed.createComponent(AppComponent);
+
+          let captcha = component.componentInstance.captcha(2, 1, 1, 2);
+
+          expect(captcha.rightOperand).toEqual("2");
+        }));
+
+        it("test 5 should be 5", async(() => {
+          const component = TestBed.createComponent(AppComponent);
+
+          let captcha = component.componentInstance.captcha(2, 1, 1, 5);
+
+          expect(captcha.rightOperand).toEqual("5");
+        }));
+
+      });
+
     });
+
+
 
   });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Captcha } from './captcha';
 export class AppComponent {
   title = 'app';
   public numberWorinding = { 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine" };
-  
+
   captcha = (pattern: number, leftOperand: number, operand: number, righOperand: number): Captcha => {
     let captcha = new Captcha();
     if (pattern === 1) {
@@ -17,6 +17,7 @@ export class AppComponent {
       captcha.rightOperand = this.numberToWording(righOperand);
     } else if (pattern === 2) {
       captcha.leftOperand = this.numberToWording(leftOperand);
+      captcha.rightOperand = righOperand + "";
     }
     return captcha
   }
